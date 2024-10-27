d310512b3d9de8fedc10804ebfb759ea71a44264
Maicol Della Chiesa <maicol.dellachiesa@gmail.com>
2024-10-27 14:08:27 +0000

    pushing solution 2

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -13,18 +13,18 @@ def draw_plot():
     # Create first line of best fit
     s, i, t1, t2, t3 = linregress(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])
     
-    x = np.linspace(1880, 2050, 170)
+    x = np.linspace(1880, 2050, 171)
     plt.plot(x, s*x+i, color='k', linestyle='--')
 
     # Create second line of best fit
-    x2 = np.linspace(2000, 2050, 50)
+    x2 = np.linspace(2000, 2050, 51)
     s2, i2, t1, t2, t3 = linregress(x=df[df['Year']>=2000]['Year'], y=df[df['Year']>=2000]['CSIRO Adjusted Sea Level'])
 
     plt.plot(x2, s2*x2+i2, color='r', linestyle='--')
 
     # Add labels and title
     plt.xlabel('Year')
-    plt.ylabel('Sea level (inches)')
+    plt.ylabel('Sea Level (inches)')
     plt.title('Rise in Sea Level')
     
     # Save plot and return data for testing (DO NOT MODIFY)
